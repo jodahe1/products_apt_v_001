c240fa1b53b955f49e7d3ab857576cfb2f6720e6
Yodahe Teshome <jodahe2001@gmail.com>
2025-06-01 13:28:52 +0300

    working on the service add 2 methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './entities/product.entity';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(
+        @InjectRepository(Product)
+        private readonly entityRepositor:EntityRepository<Product>,
+        private readonly em:EntityManager,
+    ){}
+
+    async findall():Promise<Product[]>{
+        return this.entityRepositor.findAll()
+    }
+
+  async findOne(id: number): Promise<Product | null> {
+    return this.entityRepositor.findOne(id)
+  }
+}
