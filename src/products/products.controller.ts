6179403f59d78bd4ba183038592b544bdd188b58
Yodahe Teshome <jodahe2001@gmail.com>
2025-06-01 19:44:37 +0300

    controllers and services added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller,Get,Post,Param,Body,Put,Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from './entities/product.entity';
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(
+        private readonly productService:ProductsService
+    ){}
+
+    @Get()
+    async findall():Promise<Product[]>
+    {
+        return this.findall()
+    }
+    @Get(':id')
+    async findone(@Param('id') id:string):Promise<Product| null > {
+        return this.productService.findOne(Number(id));
+    }
+    @Post()
+    async create(@Body() cdto:Omit<Product,'id'>):Promise<Product>{
+         return this.productService.create(cdto);
+    }
+     @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateData: Partial<Product>,
+  ): Promise<Product> {
+    return this.productService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.productService.remove(id);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
@@ -17,5 +17,34 @@ export class ProductsService {
 
   async findOne(id: number): Promise<Product | null> {
     return this.entityRepositor.findOne(id)
+  }
+
+
+   async create(productData: Omit<Product, 'id'>): Promise<Product> {
+    const product = new Product(
+      productData.name,
+      productData.description,
+      productData.price,
+      productData.stock,
+    );
+    await this.em.persistAndFlush(product);
+    return product;
+  }
+  async update(id:number,updateData:Partial<Product>):Promise<Product>{
+    const product=await this.findOne(id)
+    if(!product){
+      throw new NotFoundException("Product Not Found")
+    }
+    this.em.assign(product,updateData)
+    await this.em.flush()
+    return product;
+  }
+
+  async remove(id: number): Promise<void> {
+    const product = await this.entityRepositor.findOne(id);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    await this.em.removeAndFlush(product);
   }
 }
